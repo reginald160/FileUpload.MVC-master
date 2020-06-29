9bc6ea314e5ff29dd519eb5409b5be1b59d0fdaa
Codebits Academy <ozougwu2016@gmail.com>
2020-06-29 18:01:35 +0100

    Add project files.

---
+++ FileUpload.MVC/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.MVC.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Column(TypeName = "nvarchar(100)")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Qualification")]
+        public string HighestQualification { get; set; }
+
+        public long TrackingNum { get; set; }
+    }
+}
